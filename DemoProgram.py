6aa1e44931f457367d96d0d4c266f63c5e1e2c8c
ABRSO <67962053+ABRSO@users.noreply.github.com>
2021-09-27 21:24:07 +0530

    Update DemoProgram.py

--- DemoProgram.py
+++ DemoProgram.py
@@ -1,4 +1,5 @@
 # This is a demo piece of code. This might be used to do a little fidling of DevOps :)
-print('''Hey you
-You are finally awake.''')
-print('Dovakiin!')
+print('hello, world!')
+print('We are going to build Jenkins!')
+a = 9
+print('Value of a: ',a)
